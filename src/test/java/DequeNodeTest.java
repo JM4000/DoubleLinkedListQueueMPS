2ed5a884c977999afc2dcc2d5cd78d24ec99d8f1
Yomismo28 <arlym7b@uma.es>
2022-03-15 12:35:17 +0100

    Started DequeNodeTest

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -1,2 +1,19 @@
-public class DequeNodeTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DequeNodeTest<T> {
+    private DequeNode dequeNode;
+    T item;
+    DequeNode<T> next;
+    DequeNode<T> previous;
+
+    @Test
+    public void shouldSetItemEqualNewItem(){
+        DequeNode<Integer> node = new DequeNode<>(4, null, null);
+        node.setItem(7);
+        assertEquals(7, node.getItem());
+    }
 }
