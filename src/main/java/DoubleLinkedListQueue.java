b33075b184a095427650d610387e47194491393b
Juan Marcos Benítez Navone Colomer <juanmarcosmega@gmail.com>
2022-03-15 14:06:02 +0100

    Modified DoubleLinkedListQueue and DoubleLinkedListQueue
    Finished the rest of the method in DoubleLinkedListQueue and the pertinent tests

--- src/main/java/DoubleLinkedListQueue.java
+++ src/main/java/DoubleLinkedListQueue.java
@@ -2,12 +2,21 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue{
 
     private DequeNode firstNode;
     private DequeNode lastNode;
+    private int size;
 
     public DoubleLinkedListQueue(){
         firstNode = null;
         lastNode = null;
+        size = 0;
     }
 
+    private void sizeUp() {
+        this.size++;
+    }
+
+    private void sizeDown() {
+        this.size--;
+    }
     @Override
     public void append(DequeNode node) {
 
@@ -17,12 +26,12 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue{
             if(firstNode == null){
                 firstNode = node;
             }else{
-                lastNode.setNext(node);
                 node.setPrevious(lastNode);
+                lastNode.setNext(node);
             }
             lastNode = node;
+            sizeUp();
         }
-
     }
 
     @Override
@@ -33,35 +42,63 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue{
             if(firstNode == null){
                 lastNode = node;
             }else{
-                firstNode.setPrevious(node);
                 node.setNext(firstNode);
+                firstNode.setPrevious(node);
             }
             firstNode = node;
+            sizeUp();
         }
     }
 
     @Override
     public void deleteFirst() {
 
+        if(firstNode == null) {
+            throw new RuntimeException("Can't delete from an empty queue");
+        }else{
+            if(firstNode.isLastNode()){
+                firstNode = null;
+                lastNode = null;
+            }else{
+                firstNode.getNext().setPrevious(null);
+                firstNode = firstNode.getNext();
+            }
+
+            sizeDown();
+        }
     }
 
     @Override
     public void deleteLast() {
+        if(lastNode == null) {
+            throw new RuntimeException("Can't delete from an empty queue");
+        }else{
+            if(lastNode.isFirstNode()){
+                firstNode = null;
+                lastNode = null;
+            }else{
+                lastNode.getPrevious().setNext(null);
+                lastNode = lastNode.getPrevious();
+            }
 
+            sizeDown();
+        }
     }
 
     @Override
     public DequeNode peekFirst() {
-        return null;
+        if(firstNode == null) throw new RuntimeException("Can't peek an empty queue");
+        return firstNode;
     }
 
     @Override
     public DequeNode peekLast() {
-        return null;
+        if(lastNode == null) throw new RuntimeException("Can't peek an empty queue");
+        return lastNode;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 }
--- src/test/java/DoubleLinkedListQueueTest.java
+++ src/test/java/DoubleLinkedListQueueTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
@@ -26,7 +27,6 @@ public class DoubleLinkedListQueueTest {
 
     @Test
     public void appendOfNullListHasSameNodeAsFirstAndLast(){
-
         DequeNode node = new DequeNode<Integer>(Integer.valueOf(1), null, null);
         queue.append(node);
 
@@ -84,4 +84,125 @@ public class DoubleLinkedListQueueTest {
         assertThrows(RuntimeException.class, () -> queue.appendLeft(node));
     }
 
+    @Test
+    public void sizeOfAnEmptyQueueIs0(){
+        int expectedValue = 0;
+        int actualValue = queue.size();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void sizeThreeElementQueueIs3(){
+        DequeNode nodeOne = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+        DequeNode nodeTwo = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+        DequeNode nodeThree = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+
+        queue.append(nodeOne);
+        queue.append(nodeTwo);
+        queue.append(nodeThree);
+
+        int expectedValue = 3;
+        int actualValue = queue.size();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void deleteFirstFromAnEmptyQueueRaiseAnException(){
+        assertThrows(RuntimeException.class, () -> queue.deleteFirst());
+    }
+
+    @Test
+    public void deleteFirstWorksAsExpected(){
+        DequeNode nodeOne = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+        DequeNode expectedValue = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+        DequeNode nodeThree = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+
+        queue.append(nodeOne);
+        queue.append(expectedValue);
+        queue.append(nodeThree);
+
+        queue.deleteFirst();
+
+        assert(expectedValue.isFirstNode());
+    }
+
+    @Test
+    public void deleteFirstOnlyElementResultsInAnEmptyQueue(){
+        DequeNode nodeOne = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+
+        queue.append(nodeOne);
+
+        queue.deleteFirst();
+
+        assert(queue.size() == 0);
+    }
+
+    @Test
+    public void deleteLastFromAnEmptyQueueRaiseAnException(){
+        assertThrows(RuntimeException.class, () -> queue.deleteLast());
+    }
+
+    @Test
+    public void deleteLastWorksAsExpected(){
+        DequeNode nodeOne = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+        DequeNode expectedValue = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+        DequeNode nodeThree = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+
+        queue.append(nodeOne);
+        queue.append(expectedValue);
+        queue.append(nodeThree);
+
+        queue.deleteLast();
+
+        assert(expectedValue.isLastNode());
+    }
+
+    @Test
+    public void deleteLastOnlyElementResultsInAnEmptyQueue(){
+        DequeNode nodeOne = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+
+        queue.append(nodeOne);
+
+        queue.deleteLast();
+
+        assert(queue.size() == 0);
+    }
+
+    @Test
+    public void peekFirstInAnEmptyQueueRaisesException(){
+        assertThrows(RuntimeException.class, () -> queue.peekFirst());
+    }
+
+    @Test
+    public void peekLastInAnEmptyQueueRaisesException(){
+        assertThrows(RuntimeException.class, () -> queue.peekLast());
+    }
+
+    @Test
+    public void peekFirstWorkAsExpected(){
+        DequeNode expectedValue = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+        DequeNode nodeTwo = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+
+        queue.append(expectedValue);
+        queue.append(nodeTwo);
+
+        DequeNode actualValue = queue.peekFirst();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void peekLastWorkAsExpected(){
+        DequeNode nodeOne = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+        DequeNode expectedValue = new DequeNode<Integer>(Integer.valueOf(1), null, null);
+
+        queue.append(nodeOne);
+        queue.append(expectedValue);
+
+        DequeNode actualValue = queue.peekLast();
+
+        assertEquals(expectedValue, actualValue);
+    }
 }
